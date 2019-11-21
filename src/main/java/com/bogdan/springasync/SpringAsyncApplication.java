6527ab910e80f4316a442d5c29c1a33dd85e1dcd
bogdan.zastavnyi <bogdan.zastavnyi@irdeto.com>
2019-11-22 00:08:50 +0100

    async execution using annotation

--- src/main/java/com/bogdan/springasync/SpringAsyncApplication.java
+++ src/main/java/com/bogdan/springasync/SpringAsyncApplication.java
@@ -2,12 +2,30 @@ package com.bogdan.springasync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringAsyncApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAsyncApplication.class, args);
-	}
+  public static void main(String[] args) {
+    // close the application context to shut down the custom ExecutorService
+    SpringApplication.run(SpringAsyncApplication.class, args).close();
+  }
+
+  @Bean
+  public Executor taskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(10);
+    executor.setMaxPoolSize(10);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("GithubLookup-");
+    executor.initialize();
+    return executor;
+  }
 
 }
